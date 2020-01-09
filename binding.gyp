76350b90ac529f4ad77aa78ac033127c882c3910
Achronite <10363609+Achronite@users.noreply.github.com>
2020-01-09 20:45:12 +0000

    packaging updates

--- binding.gyp
+++ binding.gyp
@@ -1,7 +1,7 @@
 {
   "targets": [
     {
-      "target_name": "node_energenie_ener314rt",
+      "target_name": "ener314rt",
       "sources": ["C/achronite/napi_energenie.c","C/achronite/lock_radio.c","C/achronite/ook_send.c","C/achronite/openThings.c","C/energenie/radio.c","C/energenie/hrfm69.c","C/energenie/spis.c","C/energenie/gpio_rpi.c","C/energenie/delay_posix.c"],
     }
   ]
---
+++ index.js
@@ -0,0 +1,14 @@
+// energenie-ener314rt module: index.js entry point
+const addon = require('./build/Release/ener314rt.node');
+
+// map to functions provided in N-API .node module; allowing editors to autocomplete :)
+module.exports.initEner314rt           = addon.initEner314rt;           // Initialise radio adaptor
+module.exports.openThingsSwitch        = addon.openThingsSwitch;        // Switch an FSK device
+module.exports.openThingsDeviceList    = addon.openThingsDeviceList;    // List discovered devices
+module.exports.openThingsReceive       = addon.openThingsReceive;       // Get single message
+module.exports.openThingsReceiveThread = addon.openThingsReceiveThread; // Start Receive Thread
+module.exports.openThingsCacheCmd      = addon.openThingsCacheCmd;      // Cache an eTRV Command
+module.exports.stopMonitoring          = addon.stopMonitoring;          // Stop Receive Thread
+module.exports.ookSwitch               = addon.ookSwitch;               // Switch an OOK device (zone, switchNum, switchState, xmits)
+module.exports.sendRadioMsg            = addon.sendRadioMsg;            // Send raw payload(modulation, xmits, buffer)
+module.exports.closeEner314rt          = addon.closeEner314rt;          // Stop using the radio adaptor
